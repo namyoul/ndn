b501f0544a94923d9b1d7441ae824190702ee58e
김두현 <@KimDH-PC>
2012-05-26 16:49:43 +0900

    텝 나눴음

--- menurandom/src/com/ndn/menurandom/MainTab2Activity.java
+++ menurandom/src/com/ndn/menurandom/MainTab2Activity.java
@@ -3,7 +3,7 @@ package com.ndn.menurandom;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MainTabActivity extends Activity {
+public class MainTab2Activity extends Activity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
---
+++ menurandom/src/com/ndn/menurandom/MainTab3Activity.java
@@ -0,0 +1,15 @@
+package com.ndn.menurandom;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainTab3Activity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+	}
+
+}
