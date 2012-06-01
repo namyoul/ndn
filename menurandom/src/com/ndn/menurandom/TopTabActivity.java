850db206e53f9198d35618d4ef4662b93a39922f
reaperes <reaperes105@gmail.com>
2012-06-01 11:47:04 +0900

    TobTabActivity 를 메인으로 변경 OnTabChangeListener Method 추가

--- menurandom/src/com/ndn/menurandom/TopTabActivity.java
+++ menurandom/src/com/ndn/menurandom/TopTabActivity.java
@@ -1,14 +1,17 @@
 package com.ndn.menurandom;
 
 import android.app.TabActivity;
+import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import android.util.Log;
 import android.widget.TabHost;
+import android.widget.TabHost.OnTabChangeListener;
 import android.widget.Toast;
 import android.widget.TabHost.TabSpec;
 
-public class TopTabActivity extends TabActivity {
+public class TopTabActivity extends TabActivity implements TabHost.OnTabChangeListener {
     /** Called when the activity is first created. */
 	
     public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +37,8 @@ public class TopTabActivity extends TabActivity {
         TabSpec tabSpec3 = tabHost.newTabSpec("Tab3").setIndicator("세 번째 탭", img);
         tabSpec3.setContent(R.id.tab3);
         tabHost.addTab(tabSpec3);
+        
+        tabHost.setOnTabChangedListener(this);
     }
     
     protected void setSelectTab(int index)
@@ -41,7 +46,18 @@ public class TopTabActivity extends TabActivity {
     	TabHost tabHost = getTabHost();
     	tabHost.setCurrentTab(index);
     }
- 	
 
-    
+	public void onTabChanged(String tabId) {
+		Log.e("NHK", "TabTabTabTabTabTabTabTabTabTabTabTabTabTabTab");
+		if (tabId == "Tab1"){
+			Intent intent = new Intent(this, MainTab1Activity.class);
+			startActivity(intent);
+		}else if (tabId == "Tab2"){
+//			Intent intent = new Intent(this, MainTab2Activity.class);
+//			startActivity(intent);
+		}else if (tabId == "Tab3"){
+			Intent intent = new Intent(this, MainTab3Activity.class);
+			startActivity(intent);
+		}
+	}
 }
