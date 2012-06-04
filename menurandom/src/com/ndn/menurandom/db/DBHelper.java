78a73c5c82c0bc420d2a26e1546ee40f3d7c888c
Administrator <Administrator@110.8.136.225>
2012-06-05 00:54:13 +0900

    db 연결

---
+++ menurandom/src/com/ndn/menurandom/db/DBHelper.java
@@ -0,0 +1,37 @@
+package com.ndn.menurandom.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(Context context) {
+        super(context, "dbmenu.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) { 
+    	/*
+        String table = 
+            "CREATE TABLE menu (" +
+            "id INTEGER PRIMARY KEY AUTOINCREMENT, " + 
+            "code TEXT NOT NULL, " +
+            "detailCode TEXT , " +
+            "menuName TEXT NOT NULL, " +
+            "pictureName TEXT NOT NULL, " +
+            "snow TEXT NOT NULL, " +
+            "rain TEXT NOT NULL, " +
+            "hot TEXT NOT NULL, " +
+	        "cold TEXT NOT NULL);";
+        
+        
+        db.execSQL(table);
+        */
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {        
+        //db.execSQL("DROP TABLE IF EXISTS menu");
+        onCreate(db);
+    }
+}
